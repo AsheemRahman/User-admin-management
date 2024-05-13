7b79c3ac63f9b15b6c90909ff83fa197d049eb0f
Asheem Rahman <mhdasheem@gmail.com>
2024-05-13 13:39:31 +0530

    Delect User then User will redirect to login page

--- controller/userController/homeController.js
+++ controller/userController/homeController.js
@@ -1,11 +1,31 @@
-const home=(req,res)=>{
+// const home=(req,res)=>{
+//     try {
+//         res.render('user/home',{title:"Home",alertMessage:req.flash('errorMessage')})
+//     } catch (err) {
+//         console.log(`Error during home page render ${err}`);
+//     }
+// }
+
+
+// module.exports={
+//     home
+// }
+
+
+const userSchema = require("../../model/userSchema");
+
+const home=async (req,res)=>{
     try {
+        const userDetails=await userSchema.findOne({email:req.session.user})
+        if(userDetails===null){
+            req.session.user=""
+            return res.redirect('/user/login')
+        }
         res.render('user/home',{title:"Home",alertMessage:req.flash('errorMessage')})
     } catch (err) {
         console.log(`Error during home page render ${err}`);
-    }
+    }   
 }
-
 
 module.exports={
     home
